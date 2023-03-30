275b1c0c9a66f50863f98695f2c52f64581502c5
Mikus <pan_bananek@wp.pl>
2023-03-30 08:13:20 +0200

    Create Pionek.cs

---
+++ WarcabyTrue/WarcabyTrue/WarcabyTrue/Pionek.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarcabyTrue
+{
+    public class Pionek
+    {
+        public int Id;
+        public int Kolor;
+        public bool Zbity;
+        public Pionek() { }
+
+        public Pionek(int id, int kolor, bool zbity)
+        {
+            Id = id;
+            Kolor = kolor;
+            Zbity = zbity;
+        }
+    }
+}
