dde52741a6e79c5231097fdd7764820eb0f05445
FilipSadzynski <91310850+FilipSadzynski@users.noreply.github.com>
2023-04-03 14:06:49 +0200

    zabij

--- WarcabyTrue/WarcabyTrue/WarcabyTrue/MainPage.xaml.cs
+++ WarcabyTrue/WarcabyTrue/WarcabyTrue/MainPage.xaml.cs
@@ -71,44 +71,50 @@ namespace WarcabyTrue
             {
                 but.BorderColor = Color.Green;
                 but.Text = pion;
+                if(Math.Abs(Grid.GetColumn(but)- tempx) == 2)
+                {
+                    przyciski[tempy + (Grid.GetRow(but) - tempy)/2, tempx+(Grid.GetColumn(but) - tempx)/2].Text = null;
+                }
                 przyciski[tempy, tempx].Text = null;
                 TipClear();
             }
             else
+            {
                 TipClear();
-            tempx = Grid.GetColumn(but);
-            tempy = Grid.GetRow(but);
+                tempx = Grid.GetColumn(but);
+                tempy = Grid.GetRow(but);
 
-            if (przyciski[tempy, tempx].Text != null)
-                for (int i = -1; i <= 1; i += 2)
-                    for (int j = -1; j <= 1; j += 2)
-                    {
-                        if (tempy + i >= 0 && tempy + i < 8 && tempx + j >= 0 && tempx + j < 8 && przyciski[tempy + i, tempx + j].Text == null)
+                if (przyciski[tempy, tempx].Text != null)
+                    for (int i = -1; i <= 1; i += 2)
+                        for (int j = -1; j <= 1; j += 2)
                         {
-                            if (przyciski[tempy, tempx].Text == "😎")
-                                if (i < 0)
-                                {
-                                    pion = "😎";
-                                    przyciski[tempy + i, tempx + j].BorderColor = Color.Red;
-                                }
+                            if (tempy + i >= 0 && tempy + i < 8 && tempx + j >= 0 && tempx + j < 8 && przyciski[tempy + i, tempx + j].Text == null)
+                            {
+                                if (przyciski[tempy, tempx].Text == "😎")
+                                    if (i < 0)
+                                    {
+                                        pion = "😎";
+                                        przyciski[tempy + i, tempx + j].BorderColor = Color.Red;
+                                    }
 
-                            if (przyciski[tempy, tempx].Text == "😡")
-                                if (i > 0)
-                                {
-                                    pion = "😡";
-                                    przyciski[tempy + i, tempx + j].BorderColor = Color.Red;
-                                }
-                        }
-                        else if (tempy + i >= 0 && tempy + i < 8 && tempx + j >= 0 && tempx + j < 8 && przyciski[tempy + i, tempx + j].Text != but.Text && przyciski[tempy + i, tempx + j].Text != null)
-                        {
-                            if (tempy + (i * 2) < 8 && tempy + (i * 2) > 0 && tempx + (i * 2) < 8 && tempx + (i * 2) > 0)
-                                if (przyciski[tempy + (i * 2), tempx + (j * 2)].Text == null)
-                                {
-                                    przyciski[tempy + (i * 2), tempx + (j * 2)].BorderColor = Color.Red;
-                                }
-                        }
+                                if (przyciski[tempy, tempx].Text == "😡")
+                                    if (i > 0)
+                                    {
+                                        pion = "😡";
+                                        przyciski[tempy + i, tempx + j].BorderColor = Color.Red;
+                                    }
+                            }
+                            else if (tempy + i >= 0 && tempy + i < 8 && tempx + j >= 0 && tempx + j < 8 && przyciski[tempy + i, tempx + j].Text != but.Text && przyciski[tempy + i, tempx + j].Text != null)
+                            {
+                                if (tempy + (i * 2) < 8 && tempy + (i * 2) > 0 && tempx + (i * 2) < 8 && tempx + (i * 2) > 0)
+                                    if (przyciski[tempy + (i * 2), tempx + (j * 2)].Text == null)
+                                    {
+                                        przyciski[tempy + (i * 2), tempx + (j * 2)].BorderColor = Color.Red;
+                                    }
+                            }
 
-                    }
+                        }
+            }
         }
 
         private void TipClear()
